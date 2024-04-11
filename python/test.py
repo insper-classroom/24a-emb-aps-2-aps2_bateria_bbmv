c0854a24e63cf32711852213c06064252a664485
mariavjs <mariavjs@al.insper.edu.br>
2024-04-11 11:40:42 -0300

    python

---
+++ python/test.py
@@ -0,0 +1,25 @@
+import uinput
+import time
+
+
+# Create new mouse device
+device = uinput.Device([
+    uinput.BTN_LEFT,
+    uinput.BTN_RIGHT,
+    uinput.REL_X,
+    uinput.REL_Y,
+    uinput.KEY_SPACE,
+])
+
+
+
+
+while True:
+	time.sleep(10)
+	print('apertar')  
+	device.emit(uinput.KEY_SPACE, 1)        
+	time.sleep(2)  
+	device.emit(uinput.KEY_SPACE, 0)                         
+	
+	
+	
